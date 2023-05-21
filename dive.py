327bcd4145ab5a713ea20b0096735a01b01e6c72
Moh Ariful <106416095+moh-ariful@users.noreply.github.com>
2023-05-21 10:16:38 +0700

    Update dive.py

--- dive.py
+++ dive.py
@@ -160,7 +160,7 @@ def main():
     db.persist()
     db = None
 
-    print(f"Ingestion complete! You can now run privateGPT.py to query your documents")
+    print(f"Ingestion complete! You can now run standAloneGPT.py to query your documents")
 
 
 if __name__ == "__main__":
